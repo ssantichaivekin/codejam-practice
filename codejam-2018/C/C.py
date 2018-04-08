09bb2899a5adf8be34f5abced1fa43e04c188e6d
S.Santi Macbook <ssantichaivekin@gmail.com>
2018-04-07 18:20:36 -0700

    Finish C.

--- codejam-2018/C/C.py
+++ codejam-2018/C/C.py
@@ -1,18 +1,21 @@
 import sys
 
-if __name__ == '__main__' :
-    test_cases = int(input())
-    for case in range(1, test_cases+1) :
-        size = int(input())
+
+test_cases = int(input())
+for case in range(1, test_cases+1) :
+    size = int(input())
+    y = 15
+    x = 14
+    isDug = [[False for j in range(y)] for i in range x]
 
 
-        while x != 0 and y != 0 :
-            # print something
-            print(currentx, currenty)
-            sys.stdout.flush()
-            
-            # read something
-            y, x = list(map(int, input().split()))
-            if y == -1 and x == -1 :
-                sys.exit()
+    while x != 0 and y != 0 :
+        # print something
+        print(currentx, currenty)
+        sys.stdout.flush()
+        
+        # read something
+        y, x = list(map(int, input().split()))
+        if y == -1 and x == -1 :
+            sys.exit()
 
